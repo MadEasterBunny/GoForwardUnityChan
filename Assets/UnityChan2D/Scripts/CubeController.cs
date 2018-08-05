18d9acfc389c8ec07a56ef8103f7d5dfd721e525
Matthew Huntington Murphy <hutington67@aim.com>
2018-08-05 16:41:59 +0900

    これはGoForwardUnityChanのプロジェクトです。
    これは完成しましたGoForwardUnityChanです。

---
+++ Assets/UnityChan2D/Scripts/CubeController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+    private float speed = -0.2f;
+    private float deadLine = -10;
+
+    public AudioClip block;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Translate(this.speed, 0, 0);
+
+        if (transform.position.x < this.deadLine)
+        {
+            Destroy(gameObject);
+        }
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Ground" || collision.gameObject.tag == "Cube")
+        {
+            GetComponent<AudioSource>().PlayOneShot(block);
+        }
+    }
+}
